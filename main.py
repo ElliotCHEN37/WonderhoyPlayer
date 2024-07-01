8bb55f966f8fd6a1bd20f05191aef2e11b743cb0
Elliot <elliotchen2010@gmail.com>
2024-07-01 21:15:05 +0800

    v1.2 ready

--- main.py
+++ main.py
@@ -2,80 +2,131 @@ import sys
 import io
 import threading
 import json
-import PyQt5 as qt
+import requests
+import webbrowser
+from PyQt5 import QtWidgets, QtGui, QtCore
 from main_win import Ui_MainWindow
 from pydub import AudioSegment
 from pydub.playback import play
 
 
-class MainWindow(qt.QtWidgets.QMainWindow, Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, config):
         super().__init__()
         self.setupUi(self)
+        self.setWindowIcon(QtGui.QIcon(':/ico/wonderhoy.ico'))
 
-        self.setWindowIcon(qt.QtGui.QIcon(':/ico/wonderhoy.ico'))
-
-        wdh = qt.QtGui.QPixmap(':/img/wonderhoy.png').scaled(296, 256)
-        self.img.setIcon(qt.QtGui.QIcon(wdh))
-        self.img.setIconSize(wdh.size())
-        self.img.clicked.connect(self.play_audio)
-
-        swp = qt.QtGui.QPixmap(':/img/swap.png').scaled(100, 100)
-        self.mode_button.setIcon(qt.QtGui.QIcon(swp))
-        self.mode_button.clicked.connect(self.toggle_mode)
-
-        self.actionExit.triggered.connect(self.close)
-        self.actionAbout.triggered.connect(self.show_about_dialog)
-
+        self._setup_ui_elements()
         self.mode = int(config.get("mode", 0))
         self.audio_thread = None
         self.stop_audio_flag = threading.Event()
 
-        if self.mode == 1:
-            self.setWindowTitle(self.tr("Wonderhoy Player! - Infinite mode"))
-        else:
-            self.setWindowTitle(self.tr("Wonderhoy Player! - Regular mode"))
+        self.actionCheck_for_update.triggered.connect(self.check_for_updates)
+
+    def _setup_ui_elements(self):
+        wdh = QtGui.QPixmap(':/img/wonderhoy.png').scaled(296, 256)
+        self.img.setIcon(QtGui.QIcon(wdh))
+        self.img.setIconSize(wdh.size())
+        self.img.clicked.connect(self.play_audio)
+
+        swp = QtGui.QPixmap(':/img/swap.png').scaled(100, 100)
+        self.mode_button.setIcon(QtGui.QIcon(swp))
+        self.mode_button.clicked.connect(self.toggle_mode)
+
+        self.actionExit.triggered.connect(self.close)
+        self.actionAbout.triggered.connect(self.show_about_dialog)
+        self.spd.currentIndexChanged.connect(self.change_speed)
 
     def toggle_mode(self):
-        if self.mode == 0:
-            self.mode = 1
-            self.setWindowTitle(self.tr("Wonderhoy Player! - Infinite mode"))
-        else:
-            self.mode = 0
-            self.setWindowTitle(self.tr("Wonderhoy Player! - Regular mode"))
+        self.mode = 1 if self.mode == 0 else 0
+        self.setWindowTitle(self.tr(f"Wonderhoy Player! - {'Infinite' if self.mode else 'Regular'} mode"))
+        if self.audio_thread and self.audio_thread.is_alive():
+            self.stop_audio_flag.set()
+            self.audio_thread.join()
+            self.play_audio()
 
+    def change_speed(self):
         self.stop_audio()
+        self.play_audio()
 
     def play_audio(self):
-        if self.audio_thread is not None and self.audio_thread.is_alive():
+        if self.audio_thread and self.audio_thread.is_alive():
             self.stop_audio_flag.set()
             self.audio_thread.join()
 
         self.stop_audio_flag.clear()
 
         def play_wav():
-            audio_file = qt.QtCore.QFile(':/snd/wonderhoy.wav')
-            if audio_file.open(qt.QtCore.QFile.ReadOnly):
+            audio_file = QtCore.QFile(':/snd/wonderhoy.wav')
+            if audio_file.open(QtCore.QFile.ReadOnly):
                 audio_data = audio_file.readAll()
                 audio_file.close()
                 audio = AudioSegment.from_file(io.BytesIO(audio_data), format="wav")
-                while not self.stop_audio_flag.is_set():
-                    play(audio)
-                    if self.mode == 0:
-                        break
+
+                speed_text = self.spd.currentText()
+                try:
+                    playback_speed = float(speed_text.replace('x', ''))
+                    if playback_speed == 1.0:
+                        # Play without speed adjustment
+                        while not self.stop_audio_flag.is_set():
+                            play(audio)
+                            if self.mode == 0:
+                                break
+                    else:
+                        # Adjust playback speed
+                        audio = audio.speedup(playback_speed=playback_speed)
+                        while not self.stop_audio_flag.is_set():
+                            play(audio)
+                            if self.mode == 0:
+                                break
+
+                except ValueError:
+                    print(f"Invalid speed option: {speed_text}")
 
         self.audio_thread = threading.Thread(target=play_wav)
         self.audio_thread.start()
 
     def stop_audio(self):
         self.stop_audio_flag.set()
-        if self.audio_thread is not None:
+        if self.audio_thread:
             self.audio_thread.join()
 
     def show_about_dialog(self):
-        qt.QtWidgets.QMessageBox.about(self, self.tr("About"),
-                                       self.tr("Wonderhoy Player v1.1 by ElliotCHEN37\nLicensed under MIT License\n"
-                                               "Build time: 24/06/27"))
+        QtWidgets.QMessageBox.about(self, self.tr("About"),
+                                    self.tr("Wonderhoy Player v1.2 by ElliotCHEN37\nLicensed under MIT License\n"
+                                            "Build time: 24/07/01"))
+
+    def check_for_updates(self):
+        def fetch_remote_build():
+            try:
+                response = requests.get('https://raw.githubusercontent.com/ElliotCHEN37/WonderhoyPlayer/main/cfu.json')
+                if response.status_code == 200:
+                    remote_config = response.json()
+                    remote_build = int(remote_config.get("build", 0))
+                    local_build = 3
+                    if remote_build > local_build:
+                        QtCore.QMetaObject.invokeMethod(self, "prompt_update", QtCore.Qt.QueuedConnection)
+                    else:
+                        QtCore.QMetaObject.invokeMethod(self, "prompt_no_update", QtCore.Qt.QueuedConnection)
+            except Exception as e:
+                print(f"Failed to fetch remote build: {e}")
+
+        threading.Thread(target=fetch_remote_build).start()
+
+    @QtCore.pyqtSlot()
+    def prompt_update(self):
+        reply = QtWidgets.QMessageBox.question(self, self.tr("A new update is available"),
+                                               self.tr(
+                                                   "A new version of Wonderhoy Player is available! Do you want to update now?"),
+                                               QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                               QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            webbrowser.open("https://github.com/ElliotCHEN37/WonderhoyPlayer/releases/latest")
+
+    @QtCore.pyqtSlot()
+    def prompt_no_update(self):
+        QtWidgets.QMessageBox.information(self, self.tr("No update available"),
+                                          self.tr("You are using the latest version"))
 
     def closeEvent(self, event):
         self.stop_audio()
@@ -86,11 +137,11 @@ if __name__ == "__main__":
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    app = qt.QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
     trans_file = config.get("trans", "")
     if trans_file:
-        trans = qt.QtCore.QTranslator()
+        trans = QtCore.QTranslator()
         trans.load(trans_file)
         app.installTranslator(trans)
 
--- zh_tw.ts
+++ zh_tw.ts
@@ -4,46 +4,46 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="main_win.py" line="80"/>
+        <location filename="main_win.py" line="95"/>
         <source>Wonderhoy Player!</source>
         <translation>Wonderhoy播放器!</translation>
     </message>
     <message>
-        <location filename="main_win.py" line="81"/>
+        <location filename="main_win.py" line="96"/>
         <source>Wonderhoy! Click the sticker to play wonderhoy!
 You can change the playing mode by clicking the &apos;swap&apos; button!</source>
         <translation>Wonderhoy! 點擊貼紙以播放
 你可以通過點擊「切換」按鈕來改變模式!</translation>
     </message>
     <message>
-        <location filename="main_win.py" line="83"/>
+        <location filename="main_win.py" line="106"/>
         <source>File</source>
         <translation>檔案</translation>
     </message>
     <message>
-        <location filename="main_win.py" line="84"/>
+        <location filename="main_win.py" line="107"/>
         <source>Help</source>
         <translation>幫助</translation>
     </message>
     <message>
-        <location filename="main_win.py" line="85"/>
+        <location filename="main_win.py" line="108"/>
         <source>Exit</source>
         <translation>離開</translation>
     </message>
     <message>
-        <location filename="main_win.py" line="86"/>
+        <location filename="main_win.py" line="109"/>
         <source>About</source>
         <translation>關於</translation>
     </message>
     <message>
-        <location filename="main.py" line="42"/>
+        <location filename="main.py" line="43"/>
         <source>Wonderhoy Player! - Infinite mode</source>
-        <translation>Wonderhoy播放器 - 無限模式</translation>
+        <translation type="obsolete">Wonderhoy播放器 - 無限模式</translation>
     </message>
     <message>
-        <location filename="main.py" line="45"/>
+        <location filename="main.py" line="46"/>
         <source>Wonderhoy Player! - Regular mode</source>
-        <translation>Wonderhoy播放器 - 標準模式</translation>
+        <translation type="obsolete">Wonderhoy播放器 - 標準模式</translation>
     </message>
     <message>
         <location filename="main.py" line="73"/>
@@ -59,9 +59,93 @@ Build time: 06/26/24</source>
         <source>Wonderhoy Player v1.1 by ElliotCHEN37
 Licensed under MIT License
 Build time: 24/06/27</source>
-        <translation>Wonderhoy播放器 v1.1 由ElliotCHEN37製作
+        <translation type="obsolete">Wonderhoy播放器 v1.1 由ElliotCHEN37製作
 使用MIT授權
 組建日期: 113/06/27</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="95"/>
+        <source>Wonderhoy Player v1.2 by ElliotCHEN37
+Licensed under MIT License
+Build time: 24/07/01</source>
+        <translation>Wonderhoy播放器 v1.2 由ElliotCHEN37製作
+使用MIT授權
+組建日期: 113/07/01</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="98"/>
+        <source>Speed</source>
+        <translation>速度</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="99"/>
+        <source>1x</source>
+        <translation>1x</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="100"/>
+        <source>2x</source>
+        <translation>2x</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="101"/>
+        <source>5x</source>
+        <translation>5x</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="102"/>
+        <source>10x</source>
+        <translation>10x</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="103"/>
+        <source>100x</source>
+        <translation>100x</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="104"/>
+        <source>1000x (useless)</source>
+        <translation type="obsolete">1000倍（沒用）</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="105"/>
+        <source>Playback speed</source>
+        <translation>播放速度</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="118"/>
+        <source>A new update is available</source>
+        <translation>有可用的更新</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="118"/>
+        <source>A new version of Wonderhoy Player is available! Do you want to update now?</source>
+        <translation>有一個可用的Wonderhoy播放器更新，你想要現在更新嗎？</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="127"/>
+        <source>No update available</source>
+        <translation>無可用更新</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="127"/>
+        <source>You are using the latest version</source>
+        <translation>你正在使用最新版本</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="110"/>
+        <source>Check for update</source>
+        <translation>檢查更新</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="42"/>
+        <source>Wonderhoy Player! - {&apos;Infinite&apos; if self.mode else &apos;Regular&apos;} mode</source>
+        <translation>Wonderhoy播放器! - {'循環' if self.mode else '普通'} 模式</translation>
+    </message>
+    <message>
+        <location filename="main_win.py" line="104"/>
+        <source>1000x</source>
+        <translation>1000x</translation>
     </message>
 </context>
 </TS>
